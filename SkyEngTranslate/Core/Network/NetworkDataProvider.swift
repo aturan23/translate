08eb492118400a5f706d67a82f79420b6fb1c614
Turan Assylkhan <turan@strongte.am>
2021-03-22 19:49:01 +0600

    fix: decode from api

--- SkyEngTranslate/Core/Network/NetworkDataProvider.swift
+++ SkyEngTranslate/Core/Network/NetworkDataProvider.swift
@@ -42,31 +42,10 @@ final class NetworkDataProvider<Target: TargetType> {
         }
     }
     
-    func requestData(_ target: Target, completion: @escaping (APIResult<Data>) -> Void) {
-        guard networkReachibilityChecker?.isReachable == true else {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                completion(.failure(.networkFail))
-            }
-            return
-        }
-        dataProvider.request(target) { (result) in
-            switch result {
-            case .success(let response):
-                completion(.success(response.data))
-            case .failure(let error):
-                if let reason = error.failureReason {
-                    completion(.failure(.serverError(reason: reason)))
-                } else {
-                    completion(.failure(.unknownError))
-                }
-            }
-        }
-    }
-    
     private func decode<T: Codable>(_ response: Response) -> APIResult<T> {
         if let data = String(decoding: response.data, as: UTF8.self).data(using: .utf8),
            let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
-           let _ = jsonObject as? JSONStandard {
+           let _ = jsonObject as? [JSONStandard] {
             let withoutReasonResponse = try? response.map(T.self)
             if let response = withoutReasonResponse {
                 return .success(response)
