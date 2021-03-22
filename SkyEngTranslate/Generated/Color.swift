e54d146879c11c84ae0a3cdf7633bdc1cbc458b7
Turan Assylkhan <turan@strongte.am>
2021-03-22 19:24:53 +0600

    feat: swiftgen

---
+++ SkyEngTranslate/Generated/Color.swift
@@ -0,0 +1,33 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import UIKit
+public enum Color { }
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Color
+
+// swiftlint:disable identifier_name line_length type_body_length
+public extension Color {
+  /// 0xf1f7f9ff (r: 241, g: 247, b: 249, a: 255)
+  static let backgroundMain = #colorLiteral(red: 0.94509804, green: 0.96862745, blue: 0.9764706, alpha: 1.0)
+  /// 0xd0dbe3ff (r: 208, g: 219, b: 227, a: 255)
+  static let inputBorderRegular = #colorLiteral(red: 0.8156863, green: 0.85882354, blue: 0.8901961, alpha: 1.0)
+  /// 0xffffffff (r: 255, g: 255, b: 255, a: 255)
+  static let inputFillRegular = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+  /// 0xf5bb2eff (r: 245, g: 187, b: 46, a: 255)
+  static let mainAttention = #colorLiteral(red: 0.9607843, green: 0.73333335, blue: 0.18039216, alpha: 1.0)
+  /// 0x1d1d1dff (r: 29, g: 29, b: 29, a: 255)
+  static let mainBlack = #colorLiteral(red: 0.11372549, green: 0.11372549, blue: 0.11372549, alpha: 1.0)
+  /// 0x2d72e5ff (r: 45, g: 114, b: 229, a: 255)
+  static let mainBlue = #colorLiteral(red: 0.1764706, green: 0.44705883, blue: 0.8980392, alpha: 1.0)
+  /// 0xcf0000ff (r: 207, g: 0, b: 0, a: 255)
+  static let textError = #colorLiteral(red: 0.8117647, green: 0.0, blue: 0.0, alpha: 1.0)
+  /// 0x111111ff (r: 17, g: 17, b: 17, a: 255)
+  static let textHighContrast = #colorLiteral(red: 0.06666667, green: 0.06666667, blue: 0.06666667, alpha: 1.0)
+  /// 0x575757ff (r: 87, g: 87, b: 87, a: 255)
+  static let textLowContrast = #colorLiteral(red: 0.34117648, green: 0.34117648, blue: 0.34117648, alpha: 1.0)
+}
+// swiftlint:enable identifier_name line_length type_body_length
