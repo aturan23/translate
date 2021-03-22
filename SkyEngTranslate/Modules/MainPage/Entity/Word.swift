a9d2d67ee0a226f7a1cf9531382dfe83d4b4b46d
Turan Assylkhan <turan@strongte.am>
2021-03-22 21:41:31 +0600

    feat: modified

--- SkyEngTranslate/Modules/MainPage/Entity/Word.swift
+++ SkyEngTranslate/Modules/MainPage/Entity/Word.swift
@@ -5,6 +5,8 @@
 //  Created by Turan Assylkhan on 22.03.2021.
 //
 
+import Foundation
+
 struct Word: Codable {
     let id: Int
     let text: String
@@ -14,6 +16,21 @@ struct Word: Codable {
 struct Meaning: Codable {
     let id: Int
     let translation: Translation
+    let previewUrl: URL?
+    let transcription: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, translation, previewUrl, transcription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        translation = try container.decode(Translation.self, forKey: .translation)
+        let preview = try container.decode(String.self, forKey: .previewUrl).replacingOccurrences(of: "//", with: "http://")
+        previewUrl = URL(string: preview)
+        transcription = try container.decode(String.self, forKey: .transcription)
+    }
 }
 
 struct Translation: Codable {
