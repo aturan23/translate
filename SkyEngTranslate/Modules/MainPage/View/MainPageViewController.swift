93b0e88036e9e529cd8a39e33d1c6cea756eefd4
Turan Assylkhan <turan@strongte.am>
2021-03-22 16:54:08 +0600

    feat: setup field

--- SkyEngTranslate/Modules/MainPage/View/MainPageViewController.swift
+++ SkyEngTranslate/Modules/MainPage/View/MainPageViewController.swift
@@ -20,6 +20,8 @@ class MainPageViewController: BaseViewController, MainPageViewInput {
     // MARK: - UI components
     // ------------------------------
 
+    private let textField = TextFieldView(title: "Слово", editingActions: [.paste, .copy])
+    
     // ------------------------------
     // MARK: - Life cycle
     // ------------------------------
@@ -41,17 +43,35 @@ class MainPageViewController: BaseViewController, MainPageViewInput {
     // ------------------------------
 
     private func setupViews() {
-        view.backgroundColor = .green
+        view.backgroundColor = Color.backgroundMain
 
         setupViewsHierarchy()
         setupConstraints()
     }
 
     private func setupViewsHierarchy() {
-
+        [textField].forEach(view.addSubview(_:))
     }
 
     private func setupConstraints() {
+        textField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(LayoutGuidance.offsetThreeQuarters * 2)
+            $0.left.right.equalToSuperview().inset(LayoutGuidance.offset)
+        }
+    }
+}
+
+// ------------------------------
+// MARK: - KeyboardListening
+// ------------------------------
+
+extension MainPageViewController: KeyboardListening {
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
         
     }
 }
