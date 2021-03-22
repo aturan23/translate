948ac19624a966af9e8c01e7e6924528b697814a
Turan Assylkhan <turan@strongte.am>
2021-03-22 16:53:16 +0600

    feat: background color

--- SkyEngTranslate/Source/UIKit/Color.swift
+++ SkyEngTranslate/Source/UIKit/Color.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 enum Color {
+    static let backgroundMain = #colorLiteral(red: 0.94509804, green: 0.96862745, blue: 0.9764706, alpha: 1.0)
     static let textError = #colorLiteral(red: 0.8117647, green: 0.0, blue: 0.0, alpha: 1.0)
     static let inputBorderRegular = #colorLiteral(red: 0.8156863, green: 0.85882354, blue: 0.8901961, alpha: 1.0)
     static let textHighContrast = #colorLiteral(red: 0.06666667, green: 0.06666667, blue: 0.06666667, alpha: 1.0)
