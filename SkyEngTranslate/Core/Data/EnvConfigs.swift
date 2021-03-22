3221be03765638cccbc43ba68d16056691c6df2b
Turan Assylkhan <turan@strongte.am>
2021-03-22 19:12:44 +0600

    feat: new schemes

---
+++ SkyEngTranslate/Core/Data/EnvConfigs.swift
@@ -0,0 +1,56 @@
+//
+//  EnvConfigs.swift
+//  SkyEngTranslate
+//
+//  Created by Turan Assylkhan on 22.03.2021.
+//
+
+import Foundation
+
+public enum EnvConfigs {
+  private static let _document = PlistDocument(path: "EnvConfiguration.plist")
+
+  public static let baseUrl: String = _document["base_url"]
+}
+// swiftlint:enable identifier_name line_length type_body_length
+
+// MARK: - Implementation Details
+
+private func arrayFromPlist<T>(at path: String) -> [T] {
+  guard let url = BundleToken.bundle.url(forResource: path, withExtension: nil),
+    let data = NSArray(contentsOf: url) as? [T] else {
+    fatalError("Unable to load PLIST at path: \(path)")
+  }
+  return data
+}
+
+private struct PlistDocument {
+  let data: [String: Any]
+
+  init(path: String) {
+    guard let url = BundleToken.bundle.url(forResource: path, withExtension: nil),
+      let data = NSDictionary(contentsOf: url) as? [String: Any] else {
+        fatalError("Unable to load PLIST at path: \(path)")
+    }
+    self.data = data
+  }
+
+  subscript<T>(key: String) -> T {
+    guard let result = data[key] as? T else {
+      fatalError("Property '\(key)' is not of type \(T.self)")
+    }
+    return result
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
