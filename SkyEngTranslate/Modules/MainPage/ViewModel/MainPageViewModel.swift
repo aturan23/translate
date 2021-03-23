4da39b7e8004316580cbfd677359871fd1db0c55
Turan Assylkhan <turan@strongte.am>
2021-03-23 06:57:04 +0600

    feat: show loading logic

--- SkyEngTranslate/Modules/MainPage/ViewModel/MainPageViewModel.swift
+++ SkyEngTranslate/Modules/MainPage/ViewModel/MainPageViewModel.swift
@@ -6,10 +6,12 @@
 //  Copyright © 2021 Assylkhan Turan. All rights reserved.
 //
 
+import Foundation
+
 typealias WordsSectionModel = TableViewSectionModel<WordsCellAdapter>
 
 class MainPageViewModel: MainPageViewOutput {
-
+    
     // ------------------------------
 	// MARK: - Properties
     // ------------------------------
@@ -24,7 +26,7 @@ class MainPageViewModel: MainPageViewOutput {
     // ------------------------------
 
     func didLoad() {
-        
+        self.view?.display(viewAdapter: MainPageViewAdapter(sectionModels: []))
     }
     
     func didTapSearchButton() {
@@ -35,12 +37,17 @@ class MainPageViewModel: MainPageViewOutput {
         }
         getWords(from: text)
     }
+    
+    func didTapRetryButton() {
+        didTapSearchButton()
+    }
 
     // ------------------------------
     // MARK: - Private methods
     // ------------------------------
     
     private func getWords(from text: String) {
+        view?.showTableLoadingState()
         searchService?.getWords(text: text, completion: { [weak self] (result) in
             guard let self = self else { return }
             switch result {
@@ -48,6 +55,7 @@ class MainPageViewModel: MainPageViewOutput {
                 self.view?.display(viewAdapter: self.buildAdapter(from: response))
             case .failure(let error):
                 print(error.message)
+                self.view?.showTableFailureState()
             }
         })
     }
@@ -58,13 +66,21 @@ class MainPageViewModel: MainPageViewOutput {
     }
     
     private func buildSectionModels(with words: [Word]) -> [WordsSectionModel] {
-        words.map { word -> WordsSectionModel in
-            let adapters = word.meanings.enumerated().map { (index, item) in
-                WordsCellAdapter.makeFor(word: item, isLast: (word.meanings.count - 1) == index)
-            }
-            return WordsSectionModel(title: word.text,
-                                     items: adapters,
-                                     onSelection: {_ in})
+        words.map { word in
+            return WordsSectionModel(
+                title: word.text,
+                items: buildCellModels(from: word.meanings),
+                onSelection: didSelectRowAt(indexPath:))
+        }
+    }
+    
+    private func didSelectRowAt(indexPath: IndexPath) {
+        
+    }
+    
+    private func buildCellModels(from meanings: [Meaning]) -> [WordsCellAdapter] {
+        meanings.enumerated().map { (index, item) in
+            WordsCellAdapter.makeFor(word: item, isLast: (meanings.count - 1) == index)
         }
     }
 }
